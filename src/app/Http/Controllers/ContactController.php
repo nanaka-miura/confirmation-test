10df329df32d996f9be2ed7975ed9fc23a090843
nanaka miura <7ca.3ura@gmail.com>
2024-09-21 16:48:44 +0900

    バリデーションの修正

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -31,7 +31,7 @@ class ContactController extends Controller
         return view('confirm',compact('contact'));
     }
 
-    public function store(ContactRequest $request)
+    public function store(Request $request)
     {
 
         $contact = $request->only('category_id','content','last_name','first_name','gender','email','tel','address','building','detail');
